59fa89f09ce2433cf2b93a1233a0a8c11815d774
Aman Saleem <39592948+aman612@users.noreply.github.com>
2025-04-18 02:22:28 +0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import fastapi, uvicorn
+from starlette.requests import Request
+import prometheus_client
+import os
+
+api = fastapi.FastAPI()
+
+REQUESTS = prometheus_client.Counter(
+    "requests", "Application Request Count", ["endpoint"]
+)
+
+
+@api.get("/hello")
+def hello(request: Request):
+    REQUESTS.labels(endpoint="/hello").inc()
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    print("Starting webserver...")
+    uvicorn.run(
+        api,
+        host="0.0.0.0",
+        port=int(os.getenv("PORT", 8080)),
+        debug=os.getenv("DEBUG", False),
+        log_level=os.getenv("LOG_LEVEL", "info"),
+        proxy_headers=True,
+    )
